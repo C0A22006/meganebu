ec0cf4ee35e53b4261fb7e62971e5f0dd9004c80
okadareo <c0a2203640@edu.teu.ac.jp>
2024-02-23 15:43:16 +0000

    totyuu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # main.py
 
+from kivy.lang import Builder
+
 import os
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -20,213 +22,209 @@ from PIL import Image as PILImage
 import cv2
 from kivy.uix.camera import Camera
 
-
+#SQL初期設定
+import sqlite3
 con = sqlite3.connect("test.db")
 cur = con.cursor()
 con.execute("PRAGMA foreign_keys = true")
 
-create_table()
+#テーブル作成する関数
+def create_table():
+    try:
+        cur.execute("CREATE TABLE workbook(workbook_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);")
+    except sqlite3.OperationalError:
+        pass
+    try:
+        cur.execute("CREATE TABLE question(workbook_id INTEGER, question_id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT, answer TEXT, explanation TEXT, choise1 TEXT, choise2 TEXT, choise3 TEXT, choise4 TEXT, choise5 TEXT, choise6 TEXT, choise7 TEXT, choise8 TEXT, choise9 TEXT, FOREIGN KEY(workbook_id) references workbook(workbook_id));")
+    except sqlite3.OperationalError:
+        pass
+
+#workbookのテーブルに追加する関数
+def insert_table_workbook(name):
+    cur.execute("INSERT INTO workbook(name) values(:name);",{"name": name})
+    con.commit()
+
+#workbookのテーブルを参照する関数
+def select_table_workbook():
+    cur.execute("SELECT * FROM workbook;")
+    ans = []
+    for row in cur:
+        ans.append((row))
+    return tuple(ans)
+
+#workbookの行を削除する関数
+def delete_table_workbook(workbook_id):
+    cur.execute("DELETE FROM question WHERE workbook_id = :workbook_id",{"workbook_id": workbook_id})
+    cur.execute("DELETE FROM workbook WHERE workbook_id = :workbook_id",{"workbook_id": workbook_id})
+    con.commit()
+
+#questionのテーブルに追加する関数
+def insert_table_question(workbook_id,question,answer,explanation=None,choise1=None,choise2=None,choise3=None,choise4=None,choise5=None,choise6=None,choise7=None,choise8=None,choise9=None):
+    cur.execute("INSERT INTO question(workbook_id, question, answer, explanation, choise1, choise2, choise3, choise4, choise5, choise6, choise7, choise8, choise9) values(:workbook_id, :question, :answer, :explanation, :choise1, :choise2, :choise3, :choise4, :choise5, :choise6, :choise7, :choise8, :choise9);",{"workbook_id": workbook_id, "question": question, "answer": answer, "explanation": explanation, "choise1": choise1, "choise2": choise2, "choise3" : choise3, "choise4": choise4, "choise5": choise5, "choise6": choise6, "choise7": choise7, "choise8": choise8, "choise9": choise9})
+    con.commit()
+
+#questionのテーブルを参照する関数
+def select_table_question(workbook_id = None):
+    if workbook_id == None:
+        cur.execute("SELECT * FROM question;") 
+    else:
+        cur.execute("SELECT * FROM question where workbook_id = :workbook_id;",{"workbook_id": workbook_id})
+    ans = []
+    for row in cur:
+        ans.append((row))
+    return tuple(ans)
+
+#questionの行を削除する関数
+def delete_table_question(question_id):
+    cur.execute("DELETE FROM question WHERE question_id = :question_id",{"question_id": question_id})
+    con.commit()
+
+        
+from kivy.uix.spinner import Spinner
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+
+Window.size = (480, 720)
+
+
+#from kivy.core.window import Window
+#from kivy.uix.scrollview import ScrollView
+#from kivymd.app import MDApp
+#from kivymd.uix.list import OneLineListItem
 
 class HomeScreen(Screen):
-    def select_picture(self, instance):
-        file_path = filechooser.open_file(title="画像を選択", filters=[("画像", "*.png", "*.jpg", "*.jpeg")])
-        if file_path:
-            selected_path = file_path[0]
-            print("選択した画像:", selected_path)
-        else:
-            print("画像が選択されていません。")
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        layout = GridLayout(cols=1)
-        label = Label(text="ホーム画面", font_size=20)
-        layout.add_widget(label)
+        self.layout = GridLayout(cols=1)
+        #self.layout.bind(minimum_height=self.layout.setter('height'))
+        self.add_widget(self.layout)
         
-        button_select_picture = Button(text="写真を選択", size_hint=(None, None), size=(200, 50))
-        button_select_picture.bind(on_press=self.select_picture)
-        layout.add_widget(button_select_picture)
+        label = Label(text="ホーム", font_size=20, pos_hint=0.3, color=(0,0,0,1))
+        self.layout.add_widget(label)
+        workbook =  select_table_workbook()
+        self.count = 1
+        for i in range(len(workbook)):
+            self.spinner = Spinner(
+                text=workbook[i][1],
+                values=('問題集を作る', '問題を解く', '名前を変える','削除する'),
+                size_hint=(1, None),
+                size=(200, 50),
+                pos_hint={'center_x': .5, 'center_y': .5})
+            self.spinner.bind(text=self.show_selected_value)
+            self.layout.add_widget(self.spinner)
+            self.count += 1
+
+        self.button_add_question = Button(text="問題集を追加する", size_hint=(1, 0.3), size=(200, 50), background_color=(0.690, 0.878, 0.901, 1), background_normal='')
+        self.button_add_question.bind(on_press=self.add_buttonClicked)
+        self.layout.add_widget(self.button_add_question)
+        #root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        #root.add_widget(self.layout) 
+
+    def show_selected_value(self, spinner, choise):
+        print(self.layout)
+        if choise == '問題集を作る':
+            self.manager.current = "question_screen"
+        elif choise == '問題を解く':
+            self.manager.current = "question_screen"
+        elif choise == '名前を変える':
+            print("変える")
+        elif choise == '削除する':
+            print("削除")
         
-        self.add_widget(layout)
+        else:
+            print("eroor")
 
-    def go_to_create_quiz_screen(self, instance):
-        self.manager.current = "create_quiz_screen"
+    def add_buttonClicked(self, instance):
+        insert_table_workbook("問題集" + str(self.count))
+        self.layout.remove_widget(self.button_add_question)
+        self.spinner = Spinner(
+                text="問題集" + str(self.count),
+                values=('問題集を作る', '問題を解く', '名前を変える','削除する'),
+                size_hint=(1, None),
+                size=(200, 50),
+                pos_hint={'center_x': .5, 'center_y': .5})
+        self.spinner.bind(text=self.show_selected_value)
+        self.layout.add_widget(self.spinner)
+        self.count += 1
+        try: 
+            self.button_add_question = Button(text="問題集を追加する", size_hint=(1, 0.3), size=(200, 50))
+            self.button_add_question.bind(on_press=self.add_buttonClicked)
+            self.layout.add_widget(self.button_add_question)
 
-    def go_to_view_quiz_screen(self, instance):
-        self.manager.current = "view_quiz_screen"
+        except AttributeError:
+            pass
 
-    def go_to_select_picture(self, instance):
-        self.manager.current = "select_picture"
-
-
+    def open_buttonClicked(self, instance):
+        self.manager.current = "question_screen"
+        
 class QuestionScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        layout = GridLayout(cols=1)
-        self.add_widget(layout)
-        label = Label(text="問題文＆解答追加画面", font_size=20)
-        layout.add_widget(label)
-        self.question_input = TextInput(multiline=False, hint_text="問題文を入力してください")
-        layout.add_widget(self.question_input)
-        self.answer_input = TextInput(multiline=False, hint_text="解答を入力してください")
-        layout.add_widget(self.answer_input)
-        button_add_question = Button(text="問題文と解答を追加する", size_hint=(None, None), size=(200, 50))
-        button_add_question.bind(on_press=self.add_question_and_answer)
-        layout.add_widget(button_add_question)
-        button = Button(text="ホームへ", size_hint=(None, None), size=(200, 50))
-        button.bind(on_press=self.go_to_home_screen)
-        layout.add_widget(button)
-
-    def go_to_home_screen(self, instance):
-        self.manager.current = "home_screen"
-
-    def add_question_and_answer(self, instance):
-        question_text = self.question_input.text
-        answer_text = self.answer_input.text
-        quiz_name = self.quiz_name  # 問題集名を取得
-        if question_text and answer_text and quiz_name:
-            # 問題と解答を保存する処理
-            insert_table_question(quiz_name, question_text, answer_text)
-            print("問題と解答を追加しました")
-        else:
-            print("問題文と解答を入力してくださいまたは問題集名がありません")
-
-
-class CreateQuizScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        layout = GridLayout(cols=1)
-        self.add_widget(layout)
-        label = Label(text="問題集を作成", font_size=20)
-        layout.add_widget(label)
-        self.quiz_name_input = TextInput(multiline=False, hint_text="問題集の名前を入力してください")
-        layout.add_widget(self.quiz_name_input)
-        button_next = Button(text="次へ", size_hint=(None, None), size=(200, 50))
-        button_next.bind(on_press=self.go_to_question_screen)
-        layout.add_widget(button_next)
-        button_home = Button(text="ホームへ", size_hint=(None, None), size=(200, 50))
-        button_home.bind(on_press=self.go_to_home_screen)
-        layout.add_widget(button_home)
-
-    def go_to_home_screen(self, instance):
-        self.manager.current = "home_screen"
-
-    def go_to_question_screen(self, instance):
-        quiz_name = self.quiz_name_input.text.strip()
-        if not quiz_name:
-            print("問題集の名前を入力してください")
-            return
-        insert_table_workbook(quiz_name)
-        # Pass quiz name to the next screen
-        question_screen = self.manager.get_screen("question_screen")
-        question_screen.quiz_name = quiz_name  # 問題集名を渡す
-        self.manager.current = "question_screen"
-
-
-class ViewQuizScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        layout = GridLayout(cols=1)
-        self.add_widget(layout)
-        label = Label(text="保存した問題集を見る", font_size=20)
-        layout.add_widget(label)
-        self.quiz_content_label = Label(text="", font_size=16, size_hint_y=None)
-        self.quiz_content_label.bind(texture_size=self.quiz_content_label.setter('size'))
-        scrollview = ScrollView(size_hint=(1, None), size=(Window.width, Window.height - 200))
-        scrollview.add_widget(self.quiz_content_label)
-        layout.add_widget(scrollview)
-        # saved_quizzes ディレクトリが存在しない場合は作成する
-        if not os.path.exists("saved_quizzes"):
-            os.makedirs("saved_quizzes")
-        saved_quizzes = os.listdir('saved_quizzes')
-        for quiz_file in saved_quizzes:
-            quiz_name = quiz_file[:-4]
-            button_quiz = Button(text=quiz_name, size_hint=(None, None), size=(200, 50))
-            button_quiz.bind(on_press=lambda instance, name=quiz_name: self.view_quiz(name))
-            layout.add_widget(button_quiz)
-        button_home = Button(text="ホームへ", size_hint=(None, None), size=(200, 50))
-        button_home.bind(on_press=self.go_to_home_screen)
-        layout.add_widget(button_home)
-
-    def go_to_home_screen(self, instance):
-        self.manager.current = "home_screen"
-
-    def view_quiz(self, quiz_name):
-        # Load and display the selected quiz
-        with open(f"saved_quizzes/{quiz_name}.txt", "r") as f:
-            quiz_content = f.read()
-        self.quiz_content_label.text = quiz_content
-
-
-class ExtractQuiz(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.layout = BoxLayout(orientation='vertical')
-        self.camera = Camera(play=True)
-        self.add_widget(self.camera)
-        self.camera.bind(on_texture=self.on_camera_texture)
-        self.layout.add_widget(self.camera)
-
-        button_capture = Button(text="写真を撮る")
-        button_capture.bind(on_release=self.capture)
-        self.layout.add_widget(button_capture)
-
+        self.layout = GridLayout(cols=1)
         self.add_widget(self.layout)
+        self.button_home = Button(text="＞ホームにもどる", size_hint=(0.5, 0.1), size=(200, 50))
+        self.button_home.bind(on_press=self.home_back_buttonClicked)
+        self.layout.add_widget(self.button_home)
+        label = Label(text="問題集１", font_size=20, pos_hint=0.3, color=(0,0,0,1))
+        self.layout.add_widget(label)
+        question = select_table_question()
+        self.count = 1
+        for i in range(len(question)):
+            print(question[i][2])
+            if len(question[i][2]) > 20:
+                ans = str(question[i][2])[0:20] + "～"
+            else:
+                ans = question[i][3]
+            self.spinner = Spinner(
+                text=ans,
+                values=('削除する'),
+                size_hint=(1, None),
+                size=(200, 50),
+                pos_hint={'center_x': .5, 'center_y': .5})
+            self.spinner.bind(text=self.show_selected_value)
+            self.layout.add_widget(self.spinner)
+            self.count += 1
 
-    def on_camera_texture(self, *args):
-        pass
+        self.button_add_question = Button(text="新しい問題を作る", size_hint=(1, 0.3), size=(200, 50))
+        self.button_add_question.bind(on_press=self.create_buttonClicked)
+        self.layout.add_widget(self.button_add_question)
+        self.button_add_question = Button(text="問題を解く", size_hint=(1, 0.3), size=(200, 50))
+        self.button_add_question.bind(on_press=self.solve_buttonClicked)
+        self.layout.add_widget(self.button_add_question)
 
-    def capture(self, *args):
-        Clock.schedule_once(self.save_picture, 0.1)
-
-    def save_picture(self, *args):
-        self.camera.export_to_png("captured_image.png")
-        self.manager.current = "crop_picture"
-
-
-class CropPicture(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.layout = BoxLayout(orientation='vertical')
-        self.img = Image(source='captured_image.png')
-        self.layout.add_widget(self.img)
-        button_crop = Button(text="画像を切り抜く")
-        button_crop.bind(on_release=self.crop_image)
-        self.layout.add_widget(button_crop)
-        self.add_widget(self.layout)
-
-    def crop_image(self, *args):
-        # 画像を読み込む
-        image_path = 'captured_image.png'  # このパスは、実際に存在する画像のパスであることを確認してください。
-        img = cv2.imread(image_path)
-        if img is not None:
-            # 画像の切り取り処理をここで行う
-            # 例: imgの中心部分を切り取る
-            height, width, _ = img.shape
-            center_x, center_y = width // 2, height // 2
-            size = min(center_x, center_y)
-            cropped_img = img[center_y-size:center_y+size, center_x-size:center_x+size]
-            # 切り取った画像を保存
-            cv2.imwrite('cropped_image.png', cropped_img)
-            # 画像の更新
-            self.img.source = 'cropped_image.png'
-            self.img.reload()
+    def show_selected_value(self, spinner, choise):
+        print(self.layout)
+        if choise == '削除する':
+            print("削除") 
         else:
-            print("画像の読み込みに失敗しました。")
-        self.manager.current = "extract_quiz"
+            print("eroor")
 
+    
 
+    def home_back_buttonClicked(self, instance):
+        self.manager.current = "home_screen"
+        print("home")
+
+    def create_buttonClicked(self, instance):
+        self.manager.current = "home_screen"
+        print("create")
+
+    def solve_buttonClicked(self, instance):
+        self.manager.current = "home_screen"
+        print("solve")
+        
 
 class MainApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(HomeScreen(name="home_screen"))
-        sm.add_widget(CreateQuizScreen(name="create_quiz_screen"))
         sm.add_widget(QuestionScreen(name="question_screen"))
-        sm.add_widget(ViewQuizScreen(name="view_quiz_screen"))
-        sm.add_widget(ExtractQuiz(name="extract_quiz"))
-        sm.add_widget(CropPicture(name="crop_picture"))
         return sm
 
-
 if __name__ == "__main__":
+    create_table()
+    #insert_table_question("1", "16ビットの2進数nを16進数の各けたに分けて，下位のけたから順にスタックに格納するために，次の手順を4回", "答え", "解説", "選択肢1", "選択肢2", "選択肢3", "選択肢4", "選択肢5", "選択肢6", "選択肢7", "選択肢8")
+    #insert_table_question("1", "コトラーの競争戦略によると，業界でのシェアは高くないが，特定の製品・サービスに経営資源を集中することで，収益を高め，独自の地位", "答え", "解説", "選択肢1", "選択肢2", "選択肢3", "選択肢4", "選択肢5", "選択肢6", "選択肢7", "選択肢8")
+    #insert_table_question("2", "公開鍵暗号方式を用いて，図のようにAさんからBさんへ，他人に秘密にしておきたい文章を送るとき，暗号化に用いる鍵Kとして，適切", "答え", "解説", "選択肢1", "選択肢2", "選択肢3", "選択肢4", "選択肢5", "選択肢6", "選択肢7", "選択肢8")
+    #insert_table_question("3", "ITサービスマネジメントにおけるインシデントの記録と問題の記録の関係についての記述のうち，適切なものはどれか。", "答え", "解説", "選択肢1", "選択肢2", "選択肢3", "選択肢4", "選択肢5", "選択肢6", "選択肢7", "選択肢8")
     MainApp().run()
--- sql.py
+++ sql.py
@@ -1,98 +1,80 @@
 # sql.py
-
+#初期設定
 import sqlite3
-
-# データベース接続を開く
 con = sqlite3.connect("test.db")
+cur = con.cursor()
 con.execute("PRAGMA foreign_keys = true")
 
+#テーブル作成する関数
 def create_table():
-    # テーブルの作成
-    cur = con.cursor()
     try:
         cur.execute("CREATE TABLE workbook(workbook_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);")
-    except sqlite3.OperationalError as e:
-        print("Error creating workbook table:", e)
+    except sqlite3.OperationalError:
+        pass
     try:
-        cur.execute(
-            "CREATE TABLE question(workbook_id INTEGER, question_id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT, answer TEXT, explanation TEXT, choise1 TEXT, choise2 TEXT, choise3 TEXT, choise4 TEXT, choise5 TEXT, choise6 TEXT, choise7 TEXT, choise8 TEXT, choise9 TEXT, FOREIGN KEY(workbook_id) references workbook(workbook_id));")
-    except sqlite3.OperationalError as e:
-        print("Error creating question table:", e)
-    cur.close()
+        cur.execute("CREATE TABLE question(workbook_id INTEGER, question_id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT, answer TEXT, explanation TEXT, choise1 TEXT, choise2 TEXT, choise3 TEXT, choise4 TEXT, choise5 TEXT, choise6 TEXT, choise7 TEXT, choise8 TEXT, choise9 TEXT, FOREIGN KEY(workbook_id) references workbook(workbook_id));")
+    except sqlite3.OperationalError:
+        pass
 
+#workbookのテーブルに追加する関数
 def insert_table_workbook(name):
-    # workbookへのデータ挿入
-    cur = con.cursor()
-    cur.execute("INSERT INTO workbook(name) VALUES (?);", (name,))
+    cur.execute("INSERT INTO workbook(name) values(:name);",{"name": name})
     con.commit()
-    cur.close()
 
+#workbookのテーブルを参照する関数
 def select_table_workbook():
-    # workbookのデータ選択
-    cur = con.cursor()
     cur.execute("SELECT * FROM workbook;")
-    rows = cur.fetchall()
-    cur.close()
-    return rows
+    ans = []
+    for row in cur:
+        ans.append((row))
+    return tuple(ans)
 
+#workbookの行を削除する関数
 def delete_table_workbook(workbook_id):
-    # workbookからのデータ削除
-    cur = con.cursor()
-    cur.execute("DELETE FROM question WHERE workbook_id = ?;", (workbook_id,))
-    cur.execute("DELETE FROM workbook WHERE workbook_id = ?;", (workbook_id,))
+    cur.execute("DELETE FROM question WHERE workbook_id = :workbook_id",{"workbook_id": workbook_id})
+    cur.execute("DELETE FROM workbook WHERE workbook_id = :workbook_id",{"workbook_id": workbook_id})
     con.commit()
-    cur.close()
 
-def insert_table_question(workbook_id, question, answer, explanation=None, choise1=None, choise2=None, choise3=None, choise4=None, choise5=None, choise6=None, choise7=None, choise8=None, choise9=None):
-    # questionへのデータ挿入
-    cur = con.cursor()
-    cur.execute(
-        "INSERT INTO question(workbook_id, question, answer, explanation, choise1, choise2, choise3, choise4, choise5, choise6, choise7, choise8, choise9) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
-        (workbook_id, question, answer, explanation, choise1, choise2, choise3, choise4, choise5, choise6, choise7, choise8, choise9))
+#questionのテーブルに追加する関数
+def insert_table_question(workbook_id,question,answer,explanation=None,choise1=None,choise2=None,choise3=None,choise4=None,choise5=None,choise6=None,choise7=None,choise8=None,choise9=None):
+    cur.execute("INSERT INTO question(workbook_id, question, answer, explanation, choise1, choise2, choise3, choise4, choise5, choise6, choise7, choise8, choise9) values(:workbook_id, :question, :answer, :explanation, :choise1, :choise2, :choise3, :choise4, :choise5, :choise6, :choise7, :choise8, :choise9);",{"workbook_id": workbook_id, "question": question, "answer": answer, "explanation": explanation, "choise1": choise1, "choise2": choise2, "choise3" : choise3, "choise4": choise4, "choise5": choise5, "choise6": choise6, "choise7": choise7, "choise8": choise8, "choise9": choise9})
     con.commit()
-    cur.close()
 
-
-def select_table_question(workbook_id=None):
-    # questionのデータ選択
-    cur = con.cursor()
-    if workbook_id:
-        cur.execute("SELECT * FROM question WHERE workbook_id = ?;", (workbook_id,))
+#questionのテーブルを参照する関数
+def select_table_question(workbook_id = None):
+    row = ()
+    if workbook_id == None:
+        cur.execute("SELECT * FROM question;") 
     else:
-        cur.execute("SELECT * FROM question;")
-    rows = cur.fetchall()
-    cur.close()
-    return rows
+        cur.execute("SELECT * FROM question where workbook_id = :workbook_id;",{"workbook_id": workbook_id})
+    ans = []
+    for row in cur:
+        ans.append((row))
+    return tuple(ans)
+    
 
+#questionの行を削除する関数
 def delete_table_question(question_id):
-    # questionからのデータ削除
-    cur = con.cursor()
-    cur.execute("DELETE FROM question WHERE question_id = ?;", (question_id,))
+    cur.execute("DELETE FROM question WHERE question_id = :question_id",{"question_id": question_id})
     con.commit()
-    cur.close()
-
-if __name__ == "__main__":
-    # スクリプトが直接実行された場合、テーブルを作成する
-    create_table()
 
 #最初に行うとテーブルを作ってくれる
-create_table()
-print("---workbook---")
+#create_table()
+#print("---workbook---")
 #workbookの名前を入れる。worknook_idは自動で付与される
-insert_table_workbook("問題集")
+#insert_table_workbook("問題")
 #workbook_idを入れるとその問題集を削除する
-delete_table_workbook(2)
+#delete_table_workbook(2)
 #問題集が全部でてくる
-select_table_workbook()
+#print(select_table_workbook())
 
-print("---question---")
+#print("---question---")
 #workbook_id,問題文、答え、解説、選択肢1~9をいれる。question_idは自動で付与される
-insert_table_question("3", "問題", "答え", "解説", "選択肢1", "選択肢2", "選択肢3", "選択肢4", "選択肢5", "選択肢6", "選択肢7", "選択肢8")
+#insert_table_question("3", "問題", "答え", "解説", "選択肢1", "選択肢2", "選択肢3", "選択肢4", "選択肢5", "選択肢6", "選択肢7", "選択肢8")
 #question_idの値を入れればその問題が削除される。
-delete_table_question(6)
+#delete_table_question(6)
 #workbook_iの値を入れればその問題集のやつだけでてくる。入れなければ問題が全部でてくる。何も入ってないとNoneとでる。
-select_table_question(3)
+#print(select_table_question())
 
 #データベースを閉じる
 #cur.close()
-
